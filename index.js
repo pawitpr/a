f537f3edd91ef5dd95cc2e98c26b8148efab7c09
PawitSahare <123424956+pawitpr@users.noreply.github.com>
2023-02-10 09:35:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -104,3 +104,17 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
